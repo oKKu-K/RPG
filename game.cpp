9fca2bc71eba7181f011724741f839857e7d21a9
cgub1020 <52266168+cgub1020@users.noreply.github.com>
2019-07-19 10:53:15 +0900

    Add files via upload

--- game.cpp
+++ game.cpp
@@ -1,5 +1,10 @@
 ﻿
-//#include <iostream>
+
+// ConsoleApplication1.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+//
+
+
+//#include "stdafx.h"
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -11,25 +16,132 @@
 
 #define BUFFSIZE 1024
 
-void menu() 
+void menu()
 {
 	int x, y, w, h;
 	char str1[11] = "ダンジョン";
 	char str2[10] = "ショップ";
-	char str3[10] = "そうび";
+
 	getmaxyx(stdscr, h, w);
-	y = h/2 - 2;
-	x = (w/2 - strlen(str1)/2 - 1);
+	y = h / 2 - 2;
+	x = (w / 2 - strlen(str1) / 2 - 1);
 	mvaddstr(y, x, str1);
-	x = (w/2 - strlen(str2)/2 - 1);
-	mvaddstr(y+2, x, str2);
-	x = (w/2 - strlen(str3)/2 - 1);
-	mvaddstr(y+4, x, str3);
+	x = (w / 2 - strlen(str2) / 2 - 1);
+	mvaddstr(y + 2, x, str2);
 	refresh();
 }
 
+void dangion_list() {
+	int x, y, w, h;
+	char dan0[30] = "ダンジョンを選択してください";
+	char dan1[15] = "ゴーレムの館";
+	char dan2[15] = "トンボの巣";
+	char dan3[15] = "さいころリーグ";
+
+	getmaxyx(stdscr, h, w);
+	y = h / 2 - 6;
+	x = (w / 2 - strlen(dan0) / 2 - 1);
+	erase();
+	mvaddstr(y, x, dan0);
+	mvaddstr(y, x - 30, "戻る：B ");
+	y = h / 2 - 2;
+	x = (w / 2 - strlen(dan1) / 2 - 1);
+	mvaddstr(y, x, dan1);
+	mvaddstr(y + 2, x, dan2);
+	mvaddstr(y + 4, x, dan3);
+	refresh();
+
+}
+
+void dangion_select(){
+	int x, y, w, h;
+	int key;
+	int q = 0;
+	int c = 0;
+	char dan0[30] = "ダンジョンを選択してください";
+	char dan1[15] = "ゴーレムの館";
+	char dan2[15] = "トンボの巣";
+	char dan3[15] = "さいころリーグ";
+
+	getmaxyx(stdscr, h, w);
+	y = h / 2 - 2;
+	x = (w / 2 - strlen(dan1) / 2 - 1);
+
+	while (q == 0) {
+		attrset(COLOR_PAIR(1));
+		mvaddstr(y, x, dan1);
+		attrset(COLOR_PAIR(0));
+
+		while (q == 0) {
+			key = getch();
+			switch (key) {
+			case KEY_UP:
+				erase();
+				y = y - 2;
+				c = c - 1;
+				if (y <= h / 2 - 2) {
+					y = h / 2 - 2;
+					c = 0;
+				}
+				dangion_list();
+				attrset(COLOR_PAIR(1));
+				if (c == 0) {
+					mvaddstr(y, x, dan1);
+				}
+				else if (c == 1) {
+					mvaddstr(y, x, dan2);
+				}
+				else if (c == 2) {
+					mvaddstr(y, x, dan3);
+				}
+				attrset(COLOR_PAIR(0));
+
+				break;
+			case KEY_DOWN:
+				erase();
+				y = y + 2;
+				c = c + 1;
+				if (y >= h / 2 + 2) {
+					y = h / 2 + 2;
+					c = 2;
+				}
+				dangion_list();
+				attrset(COLOR_PAIR(1));
+				if (c == 0) {
+					mvaddstr(y, x, dan1);
+				}
+				else if (c == 1) {
+					mvaddstr(y, x, dan2);
+				}
+				else if (c == 2) {
+					mvaddstr(y, x, dan3);
+				}
+				attrset(COLOR_PAIR(0));
+				break;
+
+			case 'a':
+				erase();
+				if (y == h / 2 - 2) {
+					dangion();
+				}
+				else if (y == h / 2) {
+					dangion();
+				}
+				else if (y == h / 2 + 2) {
+					dangion();
+				}
+				break;
+			case 'b':
+				q = 1;
+				break;
+			}
+		}
+	}
+}
+
+
 void shop() {
-	int x, y, x1 ,y1 ,w, h, i;
+	int x, y, x1, y1, w, h, i;
 	int c = 0;
 	int key;
 	int q = 0;
@@ -41,7 +153,7 @@ void shop() {
 	x1 = (w / 2 - strlen(str1) / 2);
 	mvaddstr(y1, x1, str1);
 	mvprintw(y1 + 1, x1 + 30, "所持ゴールド：%dG", p.gold);
-	mvaddstr(y1 + 1, x1 - 30, "メニューに戻る：B");
+	mvaddstr(y1 + 1, x1 - 30, "メニューに戻る：B ");
 	y1 = y1 + 1;
 	x1 = (w / 2 - strlen(str2) / 2);
 	mvaddstr(y1, x1, str2);
@@ -50,10 +162,10 @@ void shop() {
 	y = h / 2 - 7;
 
 	for (i = 1; i <= 5; i++) {
-		mvprintw(y + 2*i, x, "%s:%s", shop_move[i-1].move_name, shop_move[i-1].move_exp);
+		mvprintw(y + 2 * i, x, "%s：%s", shop_move[i - 1].move_name, shop_move[i - 1].move_exp);
 	}
 	for (i = 1; i <= 5; i++) {
-		if (s[i-1] == 0) {
+		if (s[i - 1] == 0) {
 			mvaddstr(y + 2 * i, x + 55, "SOLD OUT");
 		}
 		else {
@@ -99,15 +211,15 @@ void shop() {
 			case 'a':
 				x1 = (w / 2 - strlen(str2) / 2);
 				y1 = h / 2 - 9;
-				mvaddstr(y1, x1-5, str0);
+				mvaddstr(y1, x1 - 5, str0);
 				x1 = (w / 2 - strlen(str1) / 2);
 				y1 = h / 2 - 10;
-				mvaddstr(y1, x1-5, str0);
+				mvaddstr(y1, x1 - 5, str0);
 				if (p.gold < s[c]) {
 					mvaddstr(y1, x1, "所持金が足りません");
 				}
 				else if (s[c] == 0) {
-					mvaddstr(y1, x1-1, "すでに購入しています");
+					mvaddstr(y1, x1 - 1, "すでに購入しています");
 				}
 				else {
 					p.gold = p.gold - s[c];
@@ -117,8 +229,8 @@ void shop() {
 					s[c] = 0;
 					mvprintw(y1 + 1, (w / 2 - strlen(str1) / 2) + 30, "所持ゴールド：%dG", p.gold);
 					s[c] = 0;
-					mvaddstr(y1, x1-5, str0);
-					mvaddstr(y1, x1-3, "お買い上げありがとうございます");
+					mvaddstr(y1, x1 - 5, str0);
+					mvaddstr(y1, x1 - 3, "お買い上げありがとうございます");
 					mvaddstr(y, x + 55, "SOLD OUT");
 				}
 				break;
@@ -139,21 +251,6 @@ void shop() {
 	refresh();
 }
 
-void equip() {
-	int i ,x ,y ,h ,w;
-	getmaxyx(stdscr, h, w);
-	y = h / 2 - 5;
-	x = w / 5;
-	mvaddstr(y, x+3, "そうび技");
-	mvaddstr(y, 3*x, "所持している技");
-	y = y + 2;
-	for (i = 1; i <= 6; i++) {
-		mvprintw(y + i, x, "%d：%s", i, p.m[i - 1].move_name);
-	}
-	for (i = 1; i <= count; i++) {
-		mvprintw(y + i, 3 * x - 10, "%s：%s", moveList[i-1].move_name, moveList[i-1].move_exp);
-	}
-}
 
 int main()
 {
@@ -172,10 +269,10 @@ int main()
 
 	getmaxyx(stdscr, h, w);
 
-	while(1){
+	while (1) {
 		menu();
 		x = (w / 2 - strlen("ダンジョン") / 2 - 1);
-		y = h/2 - 2 ;
+		y = h / 2 - 2;
 		attrset(COLOR_PAIR(1));
 		mvaddstr(y, x, "ダンジョン");
 		attrset(COLOR_PAIR(0));
@@ -201,10 +298,6 @@ int main()
 					x = (w / 2 - strlen("ショップ") / 2 - 1);
 					mvaddstr(y, x, "ショップ");
 				}
-				else if (c == 2) {
-					x = (w / 2 - strlen("そうび") / 2 - 1);
-					mvaddstr(y, x, "そうび");
-				}
 				attrset(COLOR_PAIR(0));
 
 				break;
@@ -212,9 +305,9 @@ int main()
 				erase();
 				y = y + 2;
 				c = c + 1;
-				if (y >= h / 2 + 2) {
-					y = h / 2 + 2;
-					c = 2;
+				if (y >= h / 2) {
+					y = h / 2;
+					c = 1;
 				}
 				menu();
 				attrset(COLOR_PAIR(1));
@@ -226,32 +319,22 @@ int main()
 					x = (w / 2 - strlen("ショップ") / 2 - 1);
 					mvaddstr(y, x, "ショップ");
 				}
-				else if (c == 2) {
-					x = (w / 2 - strlen("そうび") / 2 - 1);
-					mvaddstr(y, x, "そうび");
-				}
 				attrset(COLOR_PAIR(0));
 				break;
 
 			case 'a':
 				erase();
 				if (y == h / 2 - 2) {
-					dangion();
+					dangion_list();
+					dangion_select();
 				}
 				else if (y == h / 2) {
 					shop();
-				}
-				else {
-					equip();
 				}
 				break;
 			default:
 				break;
 			}
 		}
-
-		
-
 	}
 }
-
